1cecc5bb3fbfc698c161125850b07a06b096535a
MarianneSoares <marianneesoares@gmail.com>
2024-03-04 20:09:50 -0300

    primeiras atualizações de servico

---
+++ src/main/java/br/com/mariannesoares/servicex/categoria/domain/Categoria.java
@@ -0,0 +1,27 @@
+package br.com.mariannesoares.servicex.categoria.domain;
+
+import br.com.mariannesoares.servicex.servico.domain.Servico;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+import java.util.List;
+@Data
+@Entity
+@Table(name = "CATEGORIA")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID_CATEGORIA")
+    private Integer idCategoria;
+    @Column(name= "NOME_CATEGORIA")
+    private String nomeCategoria;
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    private List<Servico> servicos;
+
+    public Categoria(){
+
+    }
+
+
+}
---
+++ src/main/java/br/com/mariannesoares/servicex/servico/domain/Servico.java
@@ -0,0 +1,7 @@
+package br.com.mariannesoares.servicex.servico.domain;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Servico {
+}
